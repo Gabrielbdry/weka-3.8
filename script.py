f557b2815b619ca758cc4d084118b6d0693e6147
bbeaudry <b.beaudry987@gmail.com>
2019-03-13 19:59:03 -0400

    Updated python script + added new dataset

--- script.py
+++ script.py
@@ -1,10 +1,8 @@
 from threading import Thread
 import sys
-from Queue import Queue
+from queue import Queue
 import time
 import subprocess
-
-concurrent = 100
 
 def doWork():
     while True:
@@ -16,16 +14,15 @@ def getStatus(ourl):
     subprocess.call('curl -X POST "http://localhost:8081/algorithm/BayesNet" -H "accept: text/uri-list" -H "Content-Type: multipart/form-data" -F "estimator=SimpleEstimator" -F "estimatorParams=0.5" -F "searchAlgorithm=local.K2" -F "useADTree=" -F "validationNum=10" -F "searchParams=-P 1 -S BAYES" -F "datasetUri=" -F "validation=CrossValidation" -F "file=~/Documents/weka-3.8/contact-lenses.arff"', shell=True)
 
 q = Queue(0)
-for i in range(concurrent):
-    t = Thread(target=doWork)
-    t.daemon = True
-    t.start()
+t = Thread(target=doWork)
+t.daemon = True
+t.start()
 
-request_per_second = 20
+request_per_second = 1
 
 try:
     while(True):
-        for i in range(0, request_per_second * concurrent):
+        for i in range(0, request_per_second):
             q.put("task")
         time.sleep(1)
 except KeyboardInterrupt:

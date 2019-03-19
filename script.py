ccc0e9794217e0392044700517c5bf3a24e00d39
Gabriel Beaudry <gabriel.bdry@gmail.com>
2019-03-19 19:15:52 -0400

    Format script + Add jmeter files

--- script.py
+++ script.py
@@ -4,27 +4,32 @@ from queue import Queue
 import time
 import subprocess
 
-def doWork():
+
+def do_work():
     while True:
         url = q.get()
-        getStatus(url)
+        get_status()
         q.task_done()
 
-def getStatus(ourl):
-    subprocess.call('curl -X POST "http://0.0.0.0:8081/algorithm/BayesNet" -H "accept: text/uri-list" -H "Content-Type: multipart/form-data" -F "estimator=SimpleEstimator" -F "estimatorParams=0.5" -F "searchAlgorithm=local.K2" -F "useADTree=" -F "validationNum=10" -F "searchParams=-P 1 -S BAYES" -F "datasetUri=" -F "validation=CrossValidation" -F "file=@dataset_100.arff"', shell=True)
+
+def get_status():
+    subprocess.call('curl -X POST "http://0.0.0.0:8081/algorithm/BayesNet" -H "accept: text/uri-list" -H '
+                    '"Content-Type: multipart/form-data" -F "estimator=SimpleEstimator" -F "estimatorParams=0.5" -F '
+                    '"searchAlgorithm=local.K2" -F "useADTree=" -F "validationNum=10" -F "searchParams=-P 1 -S BAYES" '
+                    '-F "datasetUri=" -F "validation=CrossValidation" -F "file=@dataset_100.arff"', shell=True)
+
 
 q = Queue(0)
-t = Thread(target=doWork)
+t = Thread(target=do_work)
 t.daemon = True
 t.start()
 
 request_per_second = 1
 
 try:
-    while(True):
+    while True:
         for i in range(0, request_per_second):
             q.put("task")
         time.sleep(1)
 except KeyboardInterrupt:
     sys.exit(1)
-
